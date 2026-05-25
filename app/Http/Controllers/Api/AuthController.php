857efa2a3efd18132c295a62370e15c23a2ddcbf
relais-medical <elmediouni.san.fst@uhp.ac.ma>
2026-05-25 13:05:47 +0200

    Fix login Hash check

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -21,10 +20,10 @@ class AuthController extends Controller
     }
 
     public function login(Request $request) {
-        if (!Auth::guard('web')->attempt($request->only('email', 'password'))) {
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json(['message' => 'Identifiants incorrects'], 401);
         }
-        $user  = User::where('email', $request->email)->first();
         $token = $user->createToken('auth_token')->plainTextToken;
         return response()->json(['token' => $token, 'user' => $user]);
     }
