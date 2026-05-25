c4b5b76c7329159f595754e6ea2fec7de43d8446
relais-medical <elmediouni.san.fst@uhp.ac.ma>
2026-05-25 18:05:22 +0200

    Fix middleware stateful

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
