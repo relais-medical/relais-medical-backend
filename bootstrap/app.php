656913fb8c1a207fdb542f1119975bd7f8078df7
relais-medical <elmediouni.san.fst@uhp.ac.ma>
2026-05-25 16:55:47 +0200

    Fix middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,11 +9,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-  ->withMiddleware(function (Middleware $middleware) {
-    $middleware->use([
-        \Illuminate\Http\Middleware\HandleCors::class,
-    ]);
-})
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->statefulApi();
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
