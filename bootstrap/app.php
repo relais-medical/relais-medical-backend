0e21fdc565eb153a4a985d8116cbab31ab3ae619
relais-medical <elmediouni.san.fst@uhp.ac.ma>
2026-05-25 11:54:54 +0200

    Fix CORS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
+  ->withMiddleware(function (Middleware $middleware) {
+    $middleware->use([
+        \Illuminate\Http\Middleware\HandleCors::class,
+    ]);
+})
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
